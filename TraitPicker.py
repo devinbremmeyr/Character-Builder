507a26b54ac914d3b938ee50f922201eb5a24c06
devinbremmeyr <devinbremmeyr@gmail.com>
2018-04-30 23:34:12 -0400

    First
    Added Chacater Trait files and program rough draft

---
+++ TraitPicker.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 15 14:24:35 2018
+
+@author: Devin
+"""
+
+import random
+TraitsFile = open("Traits.txt","r")
+
+TraitsList = TraitsFile.readlines()
+
+print "Behavior: " + TraitsList[random.randint(0,TraitsList.__len__()-1)]
+
+TraitsFile.close()
+
+TraitsFile = open("Emotions.txt","r")
+
+TraitsList = TraitsFile.readlines()
+
+print "Emotion: " + TraitsList[random.randint(0,TraitsList.__len__()-1)]
+
+TraitsFile.close()
+
+TraitsFile = open("Appearance.txt","r")
+
+TraitsList = TraitsFile.readlines()
+
+print "Appearance: " + TraitsList[random.randint(0,TraitsList.__len__()-1)]
+
+TraitsFile.close()
+
+print "STR:" + str(random.randint(0,18))
+print "DEX:" + str(random.randint(0,18))
+print "CON:" + str(random.randint(0,18))
+print "INT:" + str(random.randint(0,18))
+print "WIS:" + str(random.randint(0,18))
+print "CHA:" + str(random.randint(0,18))
